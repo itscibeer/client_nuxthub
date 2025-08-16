96aff50afab1127e1e0001aa558325d0e1efbc2f
ABsolem <okijuo@gmail.com>
2025-08-16 18:49:47 +0700

    feat: add NuxtHub configuration for database and remote development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@ import { defineNuxtConfig } from "nuxt/config";
 import { resolve } from "pathe";
 
 export default defineNuxtConfig({
+  hub: {
+    database: true,
+  },
+
+  $development: {
+    hub: {
+      remote: true
+    }
+  },
+
   compatibilityDate: "2025-07-15",
 
   devtools: { enabled: true },
@@ -16,10 +26,11 @@ export default defineNuxtConfig({
     payloadExtraction: false,
   },
 
+  modules: ["@nuxthub/core"],
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3000",
-      database: true,
     },
   },
 });
